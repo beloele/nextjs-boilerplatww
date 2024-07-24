6880ff76b52615228bd41308994593a74be3df0e
belo <69517797+beloele@users.noreply.github.com>
2024-07-24 15:03:31 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import Image from "next/image";
 import Link from 'next/link';
+
 export default function Home() {
   
   return (
@@ -10,17 +11,23 @@ export default function Home() {
       <h1 className="text-4xl font-bold text-center mb-6">Starzbet Güncel Giriş</h1>
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] mb-8">
         <Link href="https://94starzbet.com" passHref>
-      <Image
-        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-        src="/logo.png"
-        alt="Starzbet logo"
-        width={540}
-        height={111}
-        priority
-      />
-  </Link>
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+            src="/logo.png"
+            alt="Starzbet logo"
+            width={540}
+            height={111}
+            priority
+          />
+        </Link>
       </div>
-      
+      <div className="mt-8">
+          <Link href="https://94starzbet.com" passHref>
+            <a className="inline-block px-8 py-4 bg-[#ff6e01] text-white font-semibold rounded-md transition-transform duration-300 transform hover:scale-105 hover:bg-[#4891ff]">
+              Starzbet Giriş İçin Tıkla
+            </a>
+          </Link>
+        </div>
       <div className="text-center max-w-2xl mx-auto">
         <section className="mb-4">
           <h2 className="text-2xl font-semibold mb-2">Starzbet Nedir?</h2>
@@ -56,6 +63,8 @@ export default function Home() {
             Starzbet, kullanıcı güvenliğini ve veri korumasını ön planda tutan bir bahis sitesidir. Modern şifreleme teknolojileri ile kullanıcı bilgilerini korur ve güvenli bir oyun ortamı sağlar. Sorumlu oyun ilkelerine bağlı kalarak, kullanıcılarına adil ve şeffaf bir oyun deneyimi sunar.
           </p>
         </section>
+        
+        
       </div>
     </main>
   );
