71ce0cf4eaffa240054e9c5b51b570735efa20f5
belo <69517797+beloele@users.noreply.github.com>
2024-07-24 14:52:39 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,28 +4,23 @@ import { useEffect } from "react";
 import Image from "next/image";
 
 export default function Home() {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      window.location.href = "https://94starzbet.com"; // Yönlendirmek istediğiniz URL
-    }, 1000); // Yönlendirme süresi 3 saniye olarak ayarlandı
-
-    return () => clearTimeout(timer); // Temizlik işlemi
-  }, []);
-
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24">
       <h1 className="text-4xl font-bold text-center mb-6">Starzbet Güncel Giriş</h1>
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] mb-8">
-        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-            src="/logo.png"
-            alt="Starzbet logo"
-            width={540}
-            height={111}
-            priority
-          />
-        </a>
+        <Link href="https://www.google.com" passHref>
+    <a target="_blank" rel="noopener noreferrer">
+      <Image
+        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+        src="/logo.png"
+        alt="Starzbet logo"
+        width={540}
+        height={111}
+        priority
+      />
+    </a>
+  </Link>
       </div>
       
       <div className="text-center max-w-2xl mx-auto">
